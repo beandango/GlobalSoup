7370d3d9ca2516d4735a363b7dc893f952676d36
Sarah Hughes <106944445+beandango@users.noreply.github.com>
2023-07-15 23:49:42 -0400

    Update Help.py

--- cogs/Help.py
+++ cogs/Help.py
@@ -13,6 +13,7 @@ class Help(commands.Cog):
             title="Commands",
             description="**/now**\ninput your timezone in all caps, and the bot returns info about your current time in this format: `<t:timestamp>`\n\n**Convert Time**\nif a message contains a time and a recognized timezone code, you can right click on the message -> Apps -> Convert Time, and the bot will display that time in the `<t:timestamp>` format\n\n**/Timezones**\nSee a full list of the currently recognized timezones\n\n**/Translate**\nInput any text and a language to get a translation of that text into that language\n\n**Translate Message**\nFirst use `/setlang` to set your preferred language, then you can right-click on any message, apps->Translate Message, and Soup will translate that message into your preferred language\n\n**/setlang**\nSets your preferred langauge to be used when using the Translate Message command above\n\n**/donate**\nThis bot uses openai's API for translation, which means it isn't free. If you like the bot, consider supporting development using this command!")
         
+        embed.set_footer(text="See code at https://github.com/beandango/GlobalSoup.git")
         await interaction.response.send_message(embed=embed)
 
 async def setup(client:commands.Bot):
